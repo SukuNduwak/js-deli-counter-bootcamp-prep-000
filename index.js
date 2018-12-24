5139275804b6a5a0525f6aecc451c536cd24e671
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-24 15:04:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function takeANumber(customerQueue, customerName) {
   var no_in_line = customerQueue.push(customerName);
   return `Welcome, customerName. You are number ${no_in_line} in line.`;
 }
+
+function nowServing(customerQueue) {
+  
+}
