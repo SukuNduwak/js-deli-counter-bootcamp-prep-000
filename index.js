de3c164a9e8f3ef5632703aacdf6cc65da2406c0
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-24 15:01:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var customerQueue = [];
 
 function takeANumber(customerQueue, customerName) {
   var no_in_line = customerQueue.push(customerName);
-  return `Welcome, customerName`;
+  return `Welcome, customerName. You are number ${no_in_line}.`;
 }
